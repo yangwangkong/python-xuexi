2268b5814c25b55469bef7624306b464d51f4d71
yyrmail123 <1137199705@qq.com>
2020-08-08 21:00:14 +0800

    first commit

---
+++ 列表查找.py
@@ -0,0 +1,17 @@
+name_list = ['tom', 'lily', 'jim']
+
+# 下标
+print(name_list)
+print(name_list[0])
+print(name_list[1])
+print(name_list[2])
+
+# 函数
+# index
+print(name_list.index('tom', 0, 2))
+print(name_list.index('jim'))
+# count
+print(name_list.count('tom'))
+print(name_list.count('toms'))
+# len
+print(len(name_list))
