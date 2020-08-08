3eb61cb2c2bd04ec495efa52324050b381836f45
yyrmail123 <1137199705@qq.com>
2020-08-08 20:28:03 +0800

    first commit

---
+++ 字符串之判断开头和结尾.py
@@ -0,0 +1,10 @@
+mystr = 'Hello world and itGdut and python'
+
+# startswith -- 判断字符串是否以某个子串开头
+print(mystr.startswith('Hello'))
+print(mystr.startswith('Hel'))
+print(mystr.startswith('Hels'))
+
+# endswith -- 判断字符串是否以某个子串结尾
+print(mystr.endswith('python'))
+print(mystr.endswith('pythons'))
