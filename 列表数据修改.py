a4e94c6fd3a9a5231de4e48edf8f7cad634c18f9
yyrmail123 <1137199705@qq.com>
2020-08-09 22:37:58 +0800

    first commit

---
+++ 列表数据修改.py
@@ -0,0 +1,14 @@
+name_list = ['tom', 'lily', 'jim']
+
+# 修改指定下标的数据
+# name_list[0] = 'aaa'
+# print(name_list)
+
+# 逆序 reverse
+list1 = [1, 5, 8, 4, 6, 7]
+# list1.reverse()
+
+# 排序 sort -- 默认升序排序
+# list1.sort()
+list1.sort(reverse=True)
+print(list1)
