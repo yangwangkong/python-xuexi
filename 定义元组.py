54cd537d8ae959d5106eed50d4a7f2c8bb0737df
yyrmail123 <1137199705@qq.com>
2020-08-12 21:13:09 +0800

    first commit

---
+++ 定义元组.py
@@ -0,0 +1,17 @@
+t1 = (10, 20, 30)
+print(type(t1))
+
+t2 = (10, )
+print(type(t2))
+
+"""
+1.元组只有一个数据也要打一个逗号在后面
+"""
+t3 = (10)
+print(type(t3))
+
+t4 = ('aaa')
+print(type(t4))
+
+t5 = ('aaa', )
+print(type(t5))
